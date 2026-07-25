83d9039379c8f41c157e9676e6c8a52d4c4bba24
shahlahaskar3-cpu <shahlahaskar3@gmail.com>
2026-07-25 10:14:02 +0530

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,6 @@
 import sys
 import os
-
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'Django_app'))
+sys.path.insert(0, os.path.dirname(__file__))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_app.settings')
-
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
